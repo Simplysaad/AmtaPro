1a9949cba3cb96c822327a3c5b7a65452d50b7dc
simplysaad <saadidris70@gmail.com>
2025-07-28 21:51:31 +0100

    fix: fixed home route to get all players

--- Backend/Routes/main.routes.js
+++ Backend/Routes/main.routes.js
@@ -8,19 +8,19 @@ import players from "../Models/players.js"
 const router = Router()
 
 
-router.post("/", async (req, res, next)=>{
-try {
-    const newPlayers = await Player.insertMany(players)
+router.get("/", async (req, res, next) => {
+  try {
+    const allPlayers = await Player.find();
 
     return res.status(201).json({
-        success: true,
-        message: "new players added",
-        data: newPlayers
-    })
-} catch (err) {
-    next(err)
-}
-})
+      success: true,
+      message: "new players added",
+      data: allPlayers,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 
 export default router;
